4d369c25cc3452ae28f0e0b006184313949a4c02
Kornel Lesiński <kornel@geekhood.net>
2014-04-05 23:30:50 +0100

    Casts

--- rwpng.c
+++ rwpng.c
@@ -150,11 +150,11 @@ static int read_chunk_callback(png_structp png_ptr, png_unknown_chunkp in_chunk)
 
     struct rwpng_chunk **head = (struct rwpng_chunk **)png_get_user_chunk_ptr(png_ptr);
 
-    struct rwpng_chunk *chunk = malloc(sizeof(struct rwpng_chunk));
+    struct rwpng_chunk *chunk = (struct rwpng_chunk *)malloc(sizeof(struct rwpng_chunk));
     memcpy(chunk->name, in_chunk->name, 5);
     chunk->size = in_chunk->size;
     chunk->location = in_chunk->location;
-    chunk->data = in_chunk->size ? malloc(in_chunk->size) : NULL;
+    chunk->data = in_chunk->size ? (unsigned char*)malloc(in_chunk->size) : NULL;
     if (in_chunk->size) {
         memcpy(chunk->data, in_chunk->data, in_chunk->size);
     }
